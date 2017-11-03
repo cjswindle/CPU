c5f7f6c3468bb2727b2b9be80f5c803574529781
cjswindle <u0487286@utah.edu>
2017-11-03 13:56:30 -0600

    added core verilog fil

---
+++ Core.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    13:16:56 11/03/2017 
+// Design Name: 
+// Module Name:    Core 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Core(
+    );
+
+
+endmodule
