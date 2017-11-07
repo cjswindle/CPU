c645a45a29d3c9a769ba8b9d277ae01eccfac76a
Jake <maschoffjake@yahoo.com>
2017-11-07 00:18:07 -0700

    started mapping each module.

--- MemoryController.v
+++ MemoryController.v
@@ -1,33 +1,35 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    13:14:41 11/03/2017 
-// Design Name: 
-// Module Name:    MemoryController 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
+// Company:
+// Engineer:
 //
-// Dependencies: 
+// Create Date:    13:14:41 11/03/2017
+// Design Name:
+// Module Name:    MemoryController
+// Project Name:
+// Target Devices:
+// Tool versions:
+// Description:
 //
-// Revision: 
+// Dependencies:
+//
+// Revision:
 // Revision 0.01 - File Created
-// Additional Comments: 
+// Additional Comments:
 //
 //////////////////////////////////////////////////////////////////////////////////
 module MemoryController(
+    input [23:0] RAMaddress,
+    input clk,
+    output [15:0] RAMdata,
+    input write_enable,
+    input [15:0] write_data,
     );
 
 
 	// still in VGA controller. Needs to be passed back and fort
-	wire req;
-	wire [7:0] VGAdata;
-	wire [13:0] RAMaddress;
-	wire [15:0] RAMdata;
-	
+
+
 	//synthesis attribute box_type VGARAM "black_box"
-	VGARAM	_VGARAM(.clka(clk),.addra(RAMaddress),.douta(RAMdata), .wea(1'b0), .dina(16'b0));
+	VGARAM	_VGARAM(.clka(clk),.addra(RAMaddress),.douta(RAMdata), .wea(write_enable), .dina(write_data));
 endmodule
